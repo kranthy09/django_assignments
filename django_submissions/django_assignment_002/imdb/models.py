a7badea510895747ee883f7e58125febf977dd79
kranthi <g.kranthi2507@gmail.com>
2020-03-12 09:57:59 +0000

    created models for movies database

--- django_submissions/django_assignment_002/imdb/models.py
+++ django_submissions/django_assignment_002/imdb/models.py
@@ -2,25 +2,27 @@ from django.db import models
 
 # Create your models here.
 
-class Movie(models.Model):
-    
-    name = models.CharField(max_length=100)
-    movie_id = models.CharField(max_length=100)
-    release_date = models.DateField()
-    box_office_collection_in_crores = models.DecimalField(decimal_places=2)
-#onyl one director directs a movie
-
 
 class Actor(models.Model):
-    actor_id  = models.CharField(max_length=100, unique=True)
+    actor_id  = models.CharField(max_length=100, primary_key=True)
     name = models.CharField(max_length=100)
-    movie = models.ManyToManyField(Movie, through="Cast")
-#ActorMovies
+    #ActorMovies
 
 
 class Director(models.Model):
-    name = models.CharField(unique=True)
-    movie = models.OneToOneField(Movie, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+
+
+
+class Movie(models.Model):
+
+    name = models.CharField(max_length=100)
+    movie_id = models.CharField(max_length=100, primary_key=True)
+    release_date = models.DateField()
+    box_office_collection_in_crores = models.DecimalField()
+    actor = models.ManyToManyField(Actor, through="Cast")
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    #onyl one director directs a movie
 
 
 class Cast(models.Model):
@@ -31,12 +33,9 @@ class Cast(models.Model):
     is_debut_movie = models.BooleanField(default=False)
 
 class Rating(models.Model):
-    RATING_CHOICES = [
-            (1, 'rating_one_count'),
-            (2, 'rating_two_count'),
-            (3, 'rating_three_count'),
-            (4, 'rating_four_count'),
-            (5, 'rating_five_count'),
-        ]
-    rating = models.IntegerField(max_length=1, choices=RATING_CHOICES, default=0)
+    rating_one_count = models.IntegerField(default=0)
+    rating_two_count = models.IntegerField(default=0)
+    rating_three_count = models.IntegerField(default=0)
+    rating_four_count = models.IntegerField(default=0)
+    rating_five_count = models.IntegerField(default=0)
     movie = models.OneToOneField(Movie, on_delete=models.CASCADE)
