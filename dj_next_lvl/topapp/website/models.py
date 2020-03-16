bedf6683ebe887c8ddff27658b44181b7c94ebea
kranthi <g.kranthi2507@gmail.com>
2020-03-16 10:14:04 +0000

    fields and lookups in detail

---
+++ dj_next_lvl/topapp/website/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Blog(models.Model):
+    
+    #table columns
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    
+    #table columns
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Entry(models.Model):
+    
+    #keys associated with the tables
+    authors = models.ManyToManyField(Author)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    
+    #table columns
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    number_of_comments = models.IntegerField()
+    number_of_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
+    
+"""
+
+entry = Entry.objects.create(blod=, 
+                             headline = "this is entry 1",
+                             body_text = "instance of Entry Class",
+                             pub_date = date(2020,2,3),
+                             mod_date = date(2020, 1,4),
+                             number_of_comments = 5,
+                             number_of_pingbacks = 7
+                            )
+
+bod.entry_set.create( 
+                             headline = "this is entry 1",
+                             body_text = "instance of Entry Class",
+                             pub_date = date(2020,2,3),
+                             mod_date = date(2020, 1,4),
+                             number_of_comments = 5,
+                             number_of_pingbacks = 7
+                            )
+"""
+
+"""
+*status*
+
+learnt till updating(save) a foreign key of a table.
+
+continue from ManyToManyField;
+"""
--- django_submissions/django_assignment_002/imdb/utils_attempt_2.py
+++ django_submissions/django_assignment_002/imdb/utils_attempt_2.py
@@ -13,9 +13,37 @@ def populate_database(actors_list, movies_list, directors_list, movie_rating_lis
     
     for movie_details in movies_list:
         
+        director_of_movie = {'director_name' : movie_details['director_name']}
         director_object = Director.objects.create(name=movie_details['director_name'])
         
+        actors_in_movie = {'actors' : movie_details['actors']}
+        
+        del movie_details['actors']
+        del movie_details['director_name']
         movie_object = Movie.objects.create(director=director_object,**movie_details)
         
+        for actors_details in actors_in_movie['actors']:
+            actor_object = Actor.objects.get(pk=actor_details['actor_id'])
+            
+            del actors_details['actor_id']
+            cast_object = Cast.objects.create(movie=movie_object, actor=actor_object,
+                                                **actor_details)
         
-    
+
+"""
+ 
+movies_list = {
+            "movie_id": "movie_1",
+            "name": "Movie 1",
+            "actors": [
+                {
+                    "actor_id": "actor_1",
+                    "role": "hero",
+                    "is_debut_movie": False
+                }
+            ],
+            "box_office_collection_in_crores": "12.3",
+            "release_date": "2020-3-3",
+            "director_name": "Director 1"
+        }
+"""
