b2f87692dd891b6e8d919b2ef7a28d93595ee436
kranthi <g.kranthi2507@gmail.com>
2020-04-25 06:52:41 +0000

    working on related objects

--- optimization/db_optimization/models.py
+++ optimization/db_optimization/models.py
@@ -31,3 +31,20 @@ class Page(models.Model):
     )
     text = models.TextField(null=True, blank=True)
     page_number = models.IntegerField()
+
+class Topping(models.Model):
+    name = models.CharField(max_length=30)
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=50)
+    toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        return "%s (%s)" % (
+            self.name,
+            ", ".join(topping.name for topping in self.toppings.all()),
+        )
+
+class Restaurant(models.Model):
+    pizzas = models.ManyToManyField(Pizza, related_name='restaurants')
+    best_pizza = models.ForeignKey(Pizza, related_name='championed_by', on_delete=models.CASCADE)
--- optimization/db_optimization/utils.py
+++ optimization/db_optimization/utils.py
@@ -19,11 +19,41 @@ def get_books_by_library_id_one_query(book_ids):
     
 @profile()
 def get_books_by_library_id(book_ids):
-    from library_app.models import Book
+    from db_optimization.models import Book
     from collections import defaultdict
     result = defaultdict(list)
    
     for book_id in book_ids:
         book = Book.objects.get(id=book_id)
         result[book.library_id].append(book)
+    return result
+
+@profile()
+def get_books_by_author():
+    from db_optimization.models import Book         
+    from collections import defaultdict    
+    books = Book.objects.all()[:30]
+    result = defaultdict(list)
+    for book in books:
+        author = book.author
+        title_and_author = '{} by {}'.format(
+            book.title,
+            author.name
+        )
+        result[book.library_id].append(title_and_author)
+    return result
+
+@profile()
+def get_books_by_author_select_related():
+    from db_optimization.models import Book
+    from collections import defaultdict
+    books = Book.objects.all().select_related('author')[:30]
+    result = defaultdict(list)
+    for book in books:
+        author = book.author
+        title_and_author = '{} by {}'.format(
+            book.title,
+            author.name
+        )
+        result[book.library_id].append(title_and_author)
     return result
